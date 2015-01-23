e7fda82ccc5d88db6b20b1f0f9fa2899c1011a3a
Aidan Steele <aidan.steele@glassechidna.com.au>
2015-01-23 14:22:22 +1100

    Add private Ruby 2.1.5 headers

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -3,6 +3,8 @@ if have_func('rb_postponed_job_register_one') &&
    have_func('rb_profile_frames') &&
    have_func('rb_tracepoint_new') &&
    have_const('RUBY_INTERNAL_EVENT_NEWOBJ')
+
+  $CFLAGS += " -I../../../../ext/ruby_headers/215"
   create_makefile('stackprof')
 else
   fail 'missing API: are you using ruby 2.1+?'
