41dbc7b00aa105a5e08d21b5d4c926895fc6d9a0
Aidan Steele <aidan.steele@glassechidna.com.au>
2015-01-23 18:06:29 +1100

    A gem that builds is even better

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,7 +4,8 @@ if have_func('rb_postponed_job_register_one') &&
    have_func('rb_tracepoint_new') &&
    have_const('RUBY_INTERNAL_EVENT_NEWOBJ')
 
-  $CFLAGS += " -I../../../../ext/ruby_headers/215"
+  ext_path = File.expand_path '../ruby_headers/215', __FILE__
+  $CFLAGS += " -I#{ext_path}"
   create_makefile('stackprofx')
 else
   fail 'missing API: are you using ruby 2.1+?'
